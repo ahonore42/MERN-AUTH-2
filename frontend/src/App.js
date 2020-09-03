87f3fb5587bf13cf21e4bfe49e1173748ee5945c
ahonore42 <a1honore80@gmail.com>
2020-09-03 18:39:25 -0400

    [boiler] add website boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Welcome from './components/Welcome'
+import About from './components/About'
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+        <Welcome />
+        
+  
+      <main>
+
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = (props) => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Animi doloribus voluptates quasi hic. Eos quasi, dignissimos 
+            ab, distinctio aspernatur minima tempore, cupiditate repellat 
+            maxime modi cum molestiae asperiores at neque?</p>
+        </div>
+    )
+}
+
+export default About
